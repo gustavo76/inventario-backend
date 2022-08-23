575e2532fde958cf4b058b3c384ba7fb65f283cc
gustavo <gustavodepaulacarreirabernardo@gmail>
2022-08-23 16:43:33 -0300

    alterado rotas

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false }));
+app.use(bodyParser.json());
+const rotaUsuarios = require('./routes/rotaUsuario');
+app.use("/usuario",rotaUsuarios);
 
-app.use("/listausuarios",(req,res,next)=>{
-
-    res.status(200).send({
-        mensagem:"deu certo!!!!",
-        nome:"carlos"
-    })
-
-
-
+app.use((req,res,next)=>{
+      const erro = new Error("Não encontrado!");
+      erro.status(404);
+      next(erro);
 });
+app.use((error,req,res,next)=>{
+        res.status(error.status || 500);
+        return res.json({
+            erro:{
+                 mensagem:error.message
+            }
+        })
+})
 
 module.exports = app
